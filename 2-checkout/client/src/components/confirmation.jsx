93d6f518af903393bd151d5e040aa30383c5bb73
Loren <loren.johnson99@yahoo.com>
2023-06-16 16:20:22 -0700

    front end sends requests to server api

--- 2-checkout/client/src/components/confirmation.jsx
+++ 2-checkout/client/src/components/confirmation.jsx
@@ -5,6 +5,7 @@ const Confirmation = ({onConfirmationNext}) => {
   return (
     <div>
       <h3>Confirm details</h3>
+      <p>render details here</p>
       <form onSubmit={onConfirmationNext}>
         <input type='submit' value='place order'></input>
       </form>
--- 2-checkout/client/src/index.jsx
+++ 2-checkout/client/src/index.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import ReactDOM from 'react-dom';
 import { render } from "react-dom";
+import axios from 'axios';
 
 import Checkout from './components/checkout.jsx';
 import UserForm from './components/userForm.jsx';
@@ -14,36 +15,80 @@ const App = () => {
   const [addressForm, setAddressForm] = useState(false);
   const [paymentForm, setPaymentForm] = useState(false);
   const [confirmation, setConfirmation] = useState(false);
+  const [orderPlaced, setOrderPlaced] = useState(false);
 
   const onCheckout = (e) => {
     e.preventDefault();
-    console.log(`checkout clicked!`);
     setHomepage(false);
     setUserForm(true);
   };
   const onUserFormNext = (e) => {
     e.preventDefault();
-    console.log(`user form completed!`);
+    let userDetails = {
+      fullName: `${e.target.children[0].value} ${e.target.children[1].value}`,
+      email: e.target.children[2].value,
+      password: e.target.children[3].value
+    }
     setUserForm(false);
     setAddressForm(true);
+    axios.post('/api', userDetails)
+      .then((response) => {
+        console.log(response)
+      })
+      .catch((err) => {
+        console.log(err);
+      })
   };
   const onAddressFormNext = (e) => {
     e.preventDefault();
-    console.log(`address form completed!`);
+    let addressDetails = {
+      addressLine1: e.target.children[0].value,
+      addressLine2: e.target.children[1].value,
+      city: e.target.children[2].value,
+      state: e.target.children[3].value,
+      zip: e.target.children[4].value,
+      phone: e.target.children[5].value
+    };
+    axios.post('/api', addressDetails)
+      .then((response) => {
+        console.log(response)
+      })
+      .catch((err) => {
+        console.log(err);
+      })
     setAddressForm(false);
     setPaymentForm(true);
   };
   const onPaymentFormNext = (e) => {
     e.preventDefault();
-    console.log(`payment form completed!`);
+    let paymentDetails = {
+      creditCard: e.target.children[0].value,
+      expiration: e.target.children[1].value,
+      CVV: e.target.children[2].value,
+      zip: e.target.children[3].value
+    };
+    axios.post('/api', paymentDetails)
+      .then((response) => {
+        console.log(response)
+      })
+      .catch((err) => {
+        console.log(err);
+      })
     setPaymentForm(false);
     setConfirmation(true);
   };
   const onConfirmationNext = (e) => {
     e.preventDefault;
-    console.log(`order successfully placed`);
+    axios.get('/api')
+      .then((response => {
+        console.log(response);
+      }))
+      .catch((err) => {
+        console.log(err);
+      });
     setConfirmation(false);
     setHomepage(true);
+    setOrderPlaced(true);
   };
 
 
@@ -59,6 +104,7 @@ const App = () => {
     {addressForm && <AddressForm onAddressFormNext={onAddressFormNext}/>}
     {paymentForm && <PaymentForm onPaymentFormNext={onPaymentFormNext}/>}
     {confirmation && <Confirmation onConfirmationNext={onConfirmationNext}/>}
+    {orderPlaced && <span> your order has been placed!</span> }
   </div>
   );
 };
